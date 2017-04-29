d9c015c79e3f0ac392e12a1f5db0220ee1d7e8b3
Luxp <986762118@qq.com>
2017-04-29 11:32:35 +0800

    第二次mac更新

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsInTheMiddleOfTypingNumber {
+            enter()
+        }
+        switch operation {
+        case "×":performOperation { $0 * $1 }
+        case "÷":performOperation { $1 / $0 }
+        case "+":performOperation { $0 + $1 }
+        case "−":performOperation { $1 - $0 }
+        case "√":performOperation { sqrt($0)}
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @nonobjc
+    func performOperation(operation: (Double) -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    var userIsInTheMiddleOfTypingNumber = false
+    @IBOutlet weak var display: UILabel!
+    @IBAction func appendDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        //print("digit = \(digit)")
+        if userIsInTheMiddleOfTypingNumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsInTheMiddleOfTypingNumber = true
+        }
+        
     }
 
-
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingNumber = false
+        operandStack.append(displayValue)
+        print(operandStack)
+    }
+    
+    var displayValue: Double {
+        get {
+            return NumberFormatter().number(from: display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingNumber = false
+        }
+    }
 }
 
