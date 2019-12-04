b90994f9d08e51d5b323b7e32e002a0cc7b19f4a
luxiaopeng <luxiaopeng@treefinance.com.cn>
2019-12-04 16:36:13 +0800

    micrometer

---
+++ micrometer/src/test/java/cn/john/lu/micrometer/MicrometerApplicationTests.java
@@ -0,0 +1,11 @@
+package cn.john.lu.micrometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class MicrometerApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
